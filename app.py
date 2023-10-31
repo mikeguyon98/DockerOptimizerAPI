e87a10ebba12e243977a334c0e6de783cf08ae98
Michael Guyon <mikeguyon98@gmail.com>
2023-10-31 17:39:19 -0400

    Adds dockerfile to analysis, modifies prompt

--- app.py
+++ app.py
@@ -1,27 +1,50 @@
 from dotenv import load_dotenv
+import os
 
+import openai
 from flask import Flask, request, jsonify
+
 from services.optimizer_service import analyze_docker_image
+from services.llm_service import generate_response
 
 app = Flask(__name__)
 
 load_dotenv()
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+prompt = "You are a software engineer specializing in Containerization. You are highly skilled in analyzing and improving Docker images using tools such as Dive"
+
 @app.route('/analyze', methods=['POST'])
 def analyze_image():
     data = request.json
-    image_name = data.get('image_name')
-    print("Image Name: ", image_name)
+    try:
+        image_name = data.get('image_name')
+        dockerfile = data.get('dockerfile')
+    except:
+        return jsonify({"error": "Invalid JSON"}), 400
+
     if not image_name:
         return jsonify({"error": "No image_name provided"}), 400
 
     analysis_results = analyze_docker_image(image_name)
-    return jsonify(analysis_results)
-    # if analysis_results:
-    #     formatted_output = format_for_llm(analysis_results)
-    #     return jsonify({"analysis": formatted_output})
-    # else:
-    #     return jsonify({"error": "Failed to analyze the image"}), 500
+    # turn analysis_results into a string
+    # send it to GPT-3
+    # return the response from GPT-3
+    if analysis_results:
+        string_results = str(analysis_results)
+        print(string_results)
+        response = generate_response(string_results, prompt, dockerfile)
+        if response:
+            return jsonify({
+                "analysis": response,
+                "stats": analysis_results['stats']
+            })
+        else:
+            return jsonify({"error": "LLM Failed to analyze image"}), 500
+    else:
+        return jsonify({"error": "Failed to generate analysis"}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ services/llm_service.py
@@ -0,0 +1,12 @@
+import openai
+
+def generate_response(content, prompt, dockerfile):
+    user_prompt = f"The following is a report generate from Dive on a docker image along with the dockerfile associated with the image. Based on this report and the dockerfile, please provide specific suggestions on how to optimize this docker image. In addition provide an example dockerfile containing the changes you would make: \n dive report: \n {content} \n dockerfile: \n {dockerfile}"
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages = [
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": user_prompt}
+        ]
+    )
+    return response.choices[0].message.content
