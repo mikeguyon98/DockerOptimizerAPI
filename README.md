2274b95e5dfed8c19aad35e6b0cb925822f13154
Michael Guyon <mikeguyon98@gmail.com>
2023-10-31 15:57:26 -0400

    finishes optimizer services core functionality

--- README.md
+++ README.md
@@ -2,12 +2,18 @@
 
 
 ## Instructions To Run Locally (for macbook):
-**Step 1**
+**Step 1** 
+
 Create or Ask Mike for .env file
 
 **Step 2**
 ```bash
-python3 -m venv flaskenv
-source flaskenv/bin/activate
+python3 -m venv flaskenv \
+source flaskenv/bin/activate \
 pip install -r requirements.txt
 ```
+
+
+## API Inputs
+- docker image name
+- Dockerfile text
--- app.py
+++ app.py
@@ -1,22 +1,27 @@
+from dotenv import load_dotenv
+
 from flask import Flask, request, jsonify
-from services.optimizer import analyze_docker_image, format_for_llm
+from services.optimizer_service import analyze_docker_image
 
 app = Flask(__name__)
+
+load_dotenv()
 
 @app.route('/analyze', methods=['POST'])
 def analyze_image():
     data = request.json
-    image_url = data.get('image_url')
-    
-    if not image_url:
-        return jsonify({"error": "No image_url provided"}), 400
+    image_name = data.get('image_name')
+    print("Image Name: ", image_name)
+    if not image_name:
+        return jsonify({"error": "No image_name provided"}), 400
 
-    analysis_results = analyze_docker_image(image_url)
-    if analysis_results:
-        formatted_output = format_for_llm(analysis_results)
-        return jsonify({"analysis": formatted_output})
-    else:
-        return jsonify({"error": "Failed to analyze the image"}), 500
+    analysis_results = analyze_docker_image(image_name)
+    return jsonify(analysis_results)
+    # if analysis_results:
+    #     formatted_output = format_for_llm(analysis_results)
+    #     return jsonify({"analysis": formatted_output})
+    # else:
+    #     return jsonify({"error": "Failed to analyze the image"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
