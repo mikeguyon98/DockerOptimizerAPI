449bd7b460d96137720fb711c65f5f3265799df5
Michael Guyon <mikeguyon98@gmail.com>
2023-11-05 22:01:00 -0500

    Upgrades to gpt-4

--- services/llm_service.py
+++ services/llm_service.py
@@ -3,7 +3,7 @@ import openai
 def generate_response(content, prompt, dockerfile):
     user_prompt = f"The following is a report generate from Dive on a docker image along with the dockerfile associated with the image. Based on this report and the dockerfile, please provide specific suggestions on how to optimize this docker image. In addition provide an example dockerfile containing the changes you would make: \n dive report: \n {content} \n dockerfile: \n {dockerfile}"
     response = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',
+        model='gpt-4-0314',
         messages = [
             {"role": "system", "content": prompt},
             {"role": "user", "content": user_prompt}
